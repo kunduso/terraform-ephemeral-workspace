414478501aa0309444149c09a904d3fd7f0cd325
Sourav Kundu <skundu.dev@gmail.com>
2025-09-21 12:45:00 -0500

    fix fir #2

---
+++ 01-workspace-management/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    organization = "wellsiau-org"
+    workspaces {
+      name = "workspace-manager"
+    }
+  }
+
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.58.0"
+    }
+  }
+  required_version = "~> 1.2"
+}
+
+provider "tfe" {
+  # Configuration will be provided via environment variables:
+  # TFE_TOKEN - HCP Terraform API token
+  # TFE_HOSTNAME - defaults to app.terraform.io
+}
