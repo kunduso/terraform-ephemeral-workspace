176a2afbe59c8666849a5c452df20d59b40aea99
sourav kundu <kunduso@amazon.com>
2025-09-25 16:07:44 -0700

    provision resources

--- 02-infrastructure/variable.tf
+++ 02-infrastructure/variable.tf
@@ -3,13 +3,3 @@ variable "region" {
   type        = string
   default     = "us-west-2"
 }
-variable "access_key" {
-  description = "The IAM access_key."
-  type        = string
-  sensitive   = true
-}
-variable "secret_key" {
-  description = "The IAM secret_key."
-  type        = string
-  sensitive   = true
-}
